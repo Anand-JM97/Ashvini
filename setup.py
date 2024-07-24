2a2617430a6a9a2b836b0a7d9ba476d18aa9f980
Balu Sreedhar <balu.srdhr@gmail.com>
2024-07-24 10:52:41 +0530

    setup.py init

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="Ashvini",
+    version="0.1",
+    author="Anand Menon",
+#    author_email="",
+    description="light-weight galaxy formation and evolution model",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    install_requires=[
+    "numpy",
+    "scipy",
+    "astropy",
+    ]
+)
