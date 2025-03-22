7ada586a6960ca929c43bdfac29122584e586c9d
Anand-JM97 <anand-jm@hotmail.com>
2025-03-22 14:26:34 +0530

    Added metallicity dependent components

--- ashvini/supernovae_feedback.py
+++ ashvini/supernovae_feedback.py
@@ -25,6 +25,15 @@ omega_L = cosmo.Ode0
 # pi_fid=1
 # epsilon_p=5
 
+def metallicity_function(stellar_metallicity, m=0.1, s=0.01, a=1, b=0.25):
+    function_value = (
+        np.exp(
+            -np.logaddexp(0, -(stellar_metallicity - m) / s)
+        )
+        * (b-a)
+        + a
+    )
+    return function_value
 
 def eta(z, m_halo, pi_fid=1, epsilon_p=5):
     eta_p = (
@@ -35,7 +44,10 @@ def eta(z, m_halo, pi_fid=1, epsilon_p=5):
     )
     return eta_p
 
-
-def m_dot_wind(m_gas, z, m_halo):
-    m_dot_wind_val = eta(z, m_halo) * sf.star_formation_rate(m_gas, z, m_halo)
+def m_dot_wind(m_gas, z, m_halo, stellar_metallicity):
+    m_dot_wind_val = (
+        eta(z, m_halo)
+        * metallicity_function(stellar_metallicity)
+        * sf.star_formation_rate(m_gas, z, m_halo)
+    )
     return m_dot_wind_val
