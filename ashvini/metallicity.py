d76754c9bb29cf7d70ffe7ecea56900b80252f58
Anand-JM97 <anand-jm@hotmail.com>
2025-03-22 15:12:30 +0530

    Started with metallicity.py

---
+++ ashvini/metallicity.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Code for metallicity evolution
+
+@author: Anand Menon, Balu Sreedhar, Chris Power
+"""
+
+import numpy as np
+from astropy.cosmology import Planck15 as cosmo
+import astropy.units as u
+
+from utils import omega_m, omega_b, omega_L, H_0
+
+from io import mdot_h, m_h
+import utils as utils
+
